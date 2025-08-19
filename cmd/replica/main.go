26eeb6e8c7c711c668e27c2f4051882defd31890
diasmashikov <diasmashikovnasa@gmail.com>
2025-08-19 19:46:47 -0400

    Add basic leader based distributed in memory data store
    From having a single read/write node we expand our architecture
    to maintain a single master for write and multiple read replicas
    to ensure horizontal scalability for reads. This simple architecture
    ensures eventual consistency by utilizing asynchronous writes to all
    replicas that are being registered in-memory in the leader's memory.
    
    Right now, we do not support:
    - leader election. So, master is our SPOF for writes and reads
    membership
    - no durable metadata for membership. If master fails, we effectively lose
    all our memberships
    - no health checks. We might send writes to dead nodes
    - we lack persistence that is needed if processes crash
    - no configurable consistency like acks or quorum
    - no retry or queue for failed replication attempts
    - no sharding to handle to scale writes horizontally
    - no proxy for client read routing or future writes
    - no consistent hashing
    - no monitoring as of now

---
+++ cmd/replica/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/diasmashikov/dedis/internal/cache"
+)
+
+var 
+( 
+	port = flag.Int("port", 8002, "http port for replica")
+	master = flag.String("master", "http://localhost:8001", "http port of the master")
+)
+
+func main() {
+	flag.Parse()
+	store := cache.New()
+
+	http.HandleFunc("/replicate", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		}
+
+		if err := r.ParseForm(); err != nil {
+            http.Error(w, "bad form", http.StatusBadRequest)
+            return
+        }
+
+		k := r.FormValue("k")
+		v := r.FormValue("v")
+
+		store.Set(k, v)
+		fmt.Fprintln(w, "ok")
+	})
+
+	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
+        k := r.URL.Query().Get("k")
+        if k == "" {
+            http.Error(w, "invalid k", http.StatusBadRequest)
+            return
+        }
+        v, ok := store.Get(k)
+        if !ok {
+            http.Error(w, "not found", http.StatusNotFound)
+            return
+        }
+        fmt.Fprintln(w, v)
+    })
+
+	go func() {
+		addr := fmt.Sprintf("localhost:%d", *port)
+		form := url.Values{"addr": {addr}}
+		client := &http.Client{Timeout: 2 * time.Second}
+		_, err := client.PostForm(*master+"/register", form)
+		if err != nil {
+			log.Printf("replicate to %s failed: %v", addr, err)
+		}
+	}()
+
+	addr := fmt.Sprintf(":%d", *port)
+	log.Printf("replica listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
